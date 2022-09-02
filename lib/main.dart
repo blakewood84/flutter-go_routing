e6161f36e095f1cdc448f1573b68ef5116cf9e7b
Blake Wood Jr <blakewoodjr84@gmail.com>
2022-09-02 09:39:05 -0700

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final GoRouter _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const MyHomePage(),
+      ),
+      GoRoute(
+        path: '/page2',
+        builder: (context, state) => const Page2(),
+        routes: [
+          GoRoute(
+            path: 'form',
+            builder: (context, state) {
+              final id = state.params['id'];
+              return Form(
+                id: id,
+              );
+            },
+          )
+        ],
+      ),
+    ],
+  );
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+    return MaterialApp.router(
+      routeInformationProvider: _router.routeInformationProvider,
+      routeInformationParser: _router.routeInformationParser,
+      routerDelegate: _router.routerDelegate,
+      title: 'Go Router Example',
     );
   }
 }
@@ -30,6 +54,99 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SizedBox(
+        height: size.height,
+        width: size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                context.go('/page2');
+              },
+              child: const Text('Page 2'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.chevron_left),
+          onPressed: () {
+            context.go('/');
+          },
+        ),
+      ),
+      body: SizedBox(
+        height: size.height,
+        width: size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                context.push('/page2/form');
+              },
+              child: const Text('Load Form'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Form extends StatelessWidget {
+  const Form({
+    required this.id,
+    Key? key,
+  }) : super(key: key);
+
+  final String? id;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Form Id: $id'),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.chevron_left),
+          onPressed: () {
+            context.pop();
+          },
+        ),
+      ),
+      body: SizedBox(
+        height: size.height,
+        width: size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                context.pop();
+              },
+              child: const Text('Back to Page 2'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
